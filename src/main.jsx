fa5bcd57d628ffca28e105045aafa5a883c8da43
bianchiluca-del <bianchi.luca@outlook.com>
2026-08-21 13:59:53 +0200

    feat: initialize automatic error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import './storage-shim.js'
 import './index.css'
 import App from './App.jsx'
 import AuthGate from './components/AuthGate.jsx'
+import AppErrorBoundary from './components/AppErrorBoundary.jsx'
+import { initMonitoring } from './lib/monitoring.js'
+
+initMonitoring()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthGate>
-      {(session, profileId, nutritionProfile, accessContext) => <App session={session} accountProfileId={profileId} nutritionProfile={nutritionProfile} accessContext={accessContext} />}
-    </AuthGate>
+    <AppErrorBoundary>
+      <AuthGate>
+        {(session, profileId, nutritionProfile, accessContext) => <App session={session} accountProfileId={profileId} nutritionProfile={nutritionProfile} accessContext={accessContext} />}
+      </AuthGate>
+    </AppErrorBoundary>
   </StrictMode>,
 )
